b3b4bfd29e6f1232019085e415bc43fab9953db1
Arkadiy <alibullterrier@gmail.com>
2023-05-31 06:45:18 +0300

    delete function

--- src/App.tsx
+++ src/App.tsx
@@ -73,18 +73,7 @@ function App() {
         }
     }
 
-    function changeTaskTitle(id: string, newTitle: string, todolistId: string) {
-        //достанем нужный массив по todolistId:
-        let todolistTasks = tasks[todolistId];
-        // найдём нужную таску:
-        let task = todolistTasks.find(t => t.id === id);
-        //изменим таску, если она нашлась
-        if (task) {
-            task.title = newTitle;
-            // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-            setTasks({...tasks});
-        }
-    }
+    
 
     function changeFilter(value: FilterValuesType, todolistId: string) {
         let todolist = todolists.find(tl => tl.id === todolistId);
