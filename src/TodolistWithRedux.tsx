d612737f6debb1c0babaa06dfaa727599cdc0cab
Arkadiy <alibullterrier@gmail.com>
2023-07-10 06:23:39 +0300

    add task

--- src/TodolistWithRedux.tsx
+++ src/TodolistWithRedux.tsx
@@ -42,10 +42,7 @@ export function TodolistWithRedux({todolist}: PropsType) {
 
     const dispatch = useDispatch()
 
-    const addTask = (title: string) => {
-        // props.addTask(title, props.id);
-        dispatch(addTaskAC(title, id))
-    };
+    
 
     const removeTodolist = () => {
         // props.removeTodolist(props.id);
